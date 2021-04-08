7ac279528cad1fa92b51fba1b918695a10fd9a49
inksharedsolutions <57968903+inksharedsolutions@users.noreply.github.com>
2021-04-08 22:53:49 +0800

    Update book-link.js
    removed Stratton Press button link

--- src/components/book-link/book-link.js
+++ src/components/book-link/book-link.js
@@ -7,17 +7,17 @@ const BookLink = (props)=>{
         <>
             <section className="fullSection">
 
-                {!Object.entries(props.data.ebooks).length === 0 && (
+                {!Object.entries(props.data.ebooks).length == 0 && (
 
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`} >
                         <span>{`${props.data.isNonfront?'Ebooks': ''}`}</span>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.amazon}>Amazon</a></li>
-                        <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>
+                        {/*<li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>*/}
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.barnes}>Barnes & Noble</a></li>
                     </nav>
                 )} 
 
-                {!Object.entries(props.data.paperback).length === 0 && (
+                {!Object.entries(props.data.paperback).length == 0 && (
 
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`} >
                         <span>{`${props.data.isNonfront?'Purchase Paperback': ''}`}</span>
@@ -27,7 +27,7 @@ const BookLink = (props)=>{
                     </nav> 
                 )}      
 
-                {!Object.entries(props.data.reviews).length === 0 && (
+                {!Object.entries(props.data.reviews).length == 0 && (
                     
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`}>
                         <span>Reviews</span>
